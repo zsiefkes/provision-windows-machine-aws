fc9f9b7ef672ec7c3d883d99f6c905f2e49d3651
Zachary Siefkes <zachary.siefkes@sentify.co>
2022-02-15 11:49:19 +1300

    Add Windows AMI, trivial build component and image recipe

--- index.ts
+++ index.ts
@@ -1,9 +1,38 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
-import * as awsx from "@pulumi/awsx";
+// import * as awsx from "@pulumi/awsx";
 
-// Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket("my-bucket");
+const buildConfig = require("./build-config.json");
 
-// Export the name of the bucket
-export const bucketName = bucket.id;
+// Base Windows AMI (copied from EC2ImageBuilder.ts). This is used in an image recipe
+const windowsBaseAmiResult = pulumi.output(aws.ec2.getAmi({
+  filters: [
+    { name: "architecture", values: ["x86_64"], },
+    { name: "platform", values: ["windows"], },
+    { name: "image-type", values: ["machine"] },
+    { name: "state", values: ["available"] },
+    { name: "virtualization-type", values: ["hvm"], },
+  ],
+  mostRecent: true,
+  owners: ["amazon"],
+  nameRegex: "^Windows_Server-2019-English-Full-Base-\\d{4}\\.\\d{2}\\.\\d{2}"
+},));
+
+// Create test build component
+const testBuildComponent = new aws.imagebuilder.Component(
+  "testTrivialBuildComponent",
+  {
+      platform: "Windows",
+      version: "0.0.1",
+      data: JSON.stringify(buildConfig)
+  }
+);
+
+// Create Image Recipe
+const imageRecipe= new aws.imagebuilder.ImageRecipe(`test-image-recipe`, {
+  parentImage: windowsBaseAmiResult.imageId,
+  components: [{
+    componentArn: testBuildComponent.arn
+  }],
+  version: "0.0.1"
+}, { deleteBeforeReplace: true });
